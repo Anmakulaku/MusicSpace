a7bd176a36b5be47e95c9cb093665ea64310c28d
Anmakulaku <anmakulaku@gmail.com>
2023-06-22 14:01:38 +0200

    Build about section

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-
+//przejście z bannera do about
 document.addEventListener('DOMContentLoaded', function() {
         const btn = document.querySelector('.banner__btn--large');
-    
+
         btn.addEventListener('click', function(event) {
         event.preventDefault();
-    
+
         const aboutSection = document.querySelector('.about');
         aboutSection.scrollIntoView({ behavior: 'smooth' });
         });
 });
+// //wyświetlanie text o about
+const button = document.querySelector('.about__btn');
+const textWrap = document.querySelector('.about__text--wrap');
+const textLong = document.querySelector('.about__text--long');
+const aboutInfo = document.querySelector('.about__info');
+
+button.addEventListener('click', function() {
+        if (textWrap.classList.contains('about__collapsed--wrap')) {
+                textWrap.classList.remove('about__collapsed--wrap');
+                textLong.classList.remove('about__collapsed--long');
+                aboutInfo.classList.remove('about__info--expanded');
+                button.textContent = 'Więcej';
+        } else {
+                textWrap.classList.add('about__collapsed--wrap');
+                textLong.classList.add('about__collapsed--long');
+                aboutInfo.classList.add('about__info--expanded');
+                button.textContent = 'Mniej';
+        }
+});
+
+
+
+
+
+
+
+
