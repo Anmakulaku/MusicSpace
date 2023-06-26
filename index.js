a5c722bed661ea11277120eb008cb1734480db7e
Anmakulaku <anmakulaku@gmail.com>
2023-06-26 15:23:02 +0200

    Add search section and scss files: variables, buttons, logo

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 //przejście z bannera do about
+const btn = document.querySelector('.banner__btn--large');
+const aboutSection = document.querySelector('.about');
+
 document.addEventListener('DOMContentLoaded', function() {
-        const btn = document.querySelector('.banner__btn--large');
-
         btn.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        const aboutSection = document.querySelector('.about');
-        aboutSection.scrollIntoView({ behavior: 'smooth' });
+                aboutSection.scrollIntoView({ behavior: 'smooth' });
         });
 });
+//kliknięcie search
+const searchInput = document.querySelector('.search__input');
+const searchButton = document.querySelector('.search__btn');
+
+// Kliknięto poza obszarem inputu i przycisku
+document.addEventListener('click', function(event) {
+        if (event.target !== searchInput && event.target !== searchButton) {
+                searchInput.style.display = 'none';
+        }
+});
+
+searchButton.addEventListener('click', function(event) {
+        event.stopPropagation(); // wyświetlanie inputu w sposób ciagły
+
+        if (searchInput.style.display === 'none') {
+                searchInput.style.display = 'inline-block';
+        } else {
+                searchInput.style.display = 'none';
+        }
+});
+
 // //wyświetlanie text o about
-const button = document.querySelector('.about__btn');
+const button = document.querySelector('.about__btn--italic');
 const textWrap = document.querySelector('.about__text--wrap');
 const textLong = document.querySelector('.about__text--long');
 const aboutInfo = document.querySelector('.about__info');
