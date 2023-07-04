c21b9f82dc7fbc65d1788e2825a815e5f7beebca
Anmakulaku <anmakulaku@gmail.com>
2023-07-04 15:29:20 +0200

    Init shop section

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ button.addEventListener('click', function() {
                 textWrap.classList.remove('about__collapsed--wrap');
                 textLong.classList.remove('about__collapsed--long');
                 aboutInfo.classList.remove('about__info--expanded');
-                button.textContent = 'Więcej';
+                button.textContent = 'WIĘCEJ';
         } else {
                 textWrap.classList.add('about__collapsed--wrap');
                 textLong.classList.add('about__collapsed--long');
                 aboutInfo.classList.add('about__info--expanded');
-                button.textContent = 'Mniej';
+                button.textContent = 'MNIEJ';
         }
 });
 
